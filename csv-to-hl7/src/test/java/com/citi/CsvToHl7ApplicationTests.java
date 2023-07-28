b89c5d836f48aad934f58991bc3544d42e2731bb
Sangram  Mule <sangram@xinifynetworks.com>
2023-07-28 12:09:51 +0530

    initial commit

---
+++ csv-to-hl7/src/test/java/com/citi/CsvToHl7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToHl7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
